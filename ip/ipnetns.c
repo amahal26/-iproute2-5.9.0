6d23685267357200bd5d145c48e9cdb02162dd24
amahal26 <harunori.amano.4z@stu.hosei.ac.jp>
2022-01-02 23:05:24 +0900

    update

--- ip/ipnetns.c
+++ ip/ipnetns.c
@@ -19,7 +19,9 @@
 #include <linux/net_namespace.h>
 
 #include "utils.h"
+#include "namespace.h"
 #include "ip_common.h"
+
 
 
 /* This socket is used to get nsid */
@@ -90,6 +92,70 @@ void netns_nsid_socket_init(void)
 		fprintf(stderr, "Cannot open rtnetlink\n");
 		exit(1);
 	}
+
+}
+
+int netns_identify_pid(const char *pidstr, char *name, int len)
+{
+	char net_path[PATH_MAX];
+	int netns = -1, ret = -1;
+	struct stat netst;
+	DIR *dir;
+	struct dirent *entry;
+
+	name[0] = '\0';
+
+	snprintf(net_path, sizeof(net_path), "/proc/%s/ns/net", pidstr);
+	netns = open(net_path, O_RDONLY);
+	if (netns < 0) {
+		fprintf(stderr, "Cannot open network namespace: %s\n",
+			strerror(errno));
+		goto out;
+	}
+	if (fstat(netns, &netst) < 0) {
+		fprintf(stderr, "Stat of netns failed: %s\n",
+			strerror(errno));
+		goto out;
+	}
+	dir = opendir(NETNS_RUN_DIR);
+	if (!dir) {
+		/* Succeed treat a missing directory as an empty directory */
+		if (errno == ENOENT) {
+			ret = 0;
+			goto out;
+		}
+
+		fprintf(stderr, "Failed to open directory %s:%s\n",
+			NETNS_RUN_DIR, strerror(errno));
+		goto out;
+	}
+
+	while ((entry = readdir(dir))) {
+		char name_path[PATH_MAX];
+		struct stat st;
+
+		if (strcmp(entry->d_name, ".") == 0)
+			continue;
+		if (strcmp(entry->d_name, "..") == 0)
+			continue;
+
+		snprintf(name_path, sizeof(name_path), "%s/%s",	NETNS_RUN_DIR,
+			entry->d_name);
+
+		if (stat(name_path, &st) != 0)
+			continue;
+
+		if ((st.st_dev == netst.st_dev) &&
+		    (st.st_ino == netst.st_ino)) {
+			strlcpy(name, entry->d_name, len);
+		}
+	}
+	ret = 0;
+	closedir(dir);
+out:
+	if (netns >= 0)
+		close(netns);
+	return ret;
 
 }
 
