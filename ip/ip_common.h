6378fcc17b8352ae88297d5be29c8c9e9f5791ac
amahal26 <harunori.amano.4z@stu.hosei.ac.jp>
2022-01-02 21:46:27 +0900

    first commit

---
+++ ip/ip_common.h
@@ -0,0 +1,60 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _IP_COMMON_H_
+#define _IP_COMMON_H_
+
+#include <stdbool.h>
+
+#include "json_print.h"
+
+struct link_filter {
+	int ifindex;
+	int family;
+	int oneline;
+	int showqueue;
+	inet_prefix pfx;
+	int scope, scopemask;
+	int flags, flagmask;
+	int up;
+	char *label;
+	int flushed;
+	char *flushb;
+	int flushp;
+	int flushe;
+	int group;
+	int master;
+	char *kind;
+	char *slave_kind;
+	int target_nsid;
+};
+
+int get_operstate(const char *name);//
+int print_linkinfo(struct nlmsghdr *n, void *arg);//
+int print_addrinfo(struct nlmsghdr *n, void *arg);//
+void ipaddr_reset_filter(int oneline, int ifindex);
+
+void netns_nsid_socket_init(void);//
+int do_ipaddr(int argc, char **argv);//
+int do_netns(int argc, char **argv);//
+
+int ip_link_list(req_filter_fn_t filter_fn, struct nlmsg_chain *linfo);//
+void free_nlmsg_chain(struct nlmsg_chain *info);
+
+extern struct rtnl_handle rth;
+
+struct iplink_req {
+	struct nlmsghdr		n;
+	struct ifinfomsg	i;
+	char			buf[1024];
+};
+
+int iplink_parse(int argc, char **argv, struct iplink_req *req, char **type);
+
+#ifndef	INFINITY_LIFE_TIME
+#define     INFINITY_LIFE_TIME      0xFFFFFFFFU
+#endif
+
+#ifndef LABEL_MAX_MASK
+#define     LABEL_MAX_MASK          0xFFFFFU
+#endif
+
+#endif /* _IP_COMMON_H_ */
